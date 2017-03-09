6a5032373f01945bd876918edf077f3647d0ae8f
johrdane93 <johrdane.ledra@lepoles.com>
2017-03-09 11:19:17 +0100

    Add files via upload

---
+++ 04-FoodTruck/inc/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>page texte</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <header>
+
+    <!-- Navigation Menu -->
+    <nav>
+      <ul>
+        <li class="inlineB"><a href="index.php">page1</a></li>
+        <li class="inlineB"><a href="SandWitch03.php">Page2</a></li>
+        <li class="inlineB"><a href="food_truckpage02.php">Page3</a></li>
+        <li class="inlineB"><a href="indexJopage01.php">Page4</a></li>
+      </ul>
+    </nav>
+  </header>
